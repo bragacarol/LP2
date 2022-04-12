528c64e83d258fbdff6e3b98809b32b0e2c9a17c
Caroline Braga <88350832+bragacarol@users.noreply.github.com>
2022-04-11 22:48:03 -0300

    Add files via upload

---
+++ Atividade 4/PTriangulo/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(206, 92);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 3;
+            this.txt1.TextChanged += new System.EventHandler(this.txt1_TextChanged);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(206, 130);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 4;
+            this.txt2.TextChanged += new System.EventHandler(this.txt2_TextChanged);
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(206, 166);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 20);
+            this.txt3.TabIndex = 5;
+            this.txt3.TextChanged += new System.EventHandler(this.txt3_TextChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn1.Location = new System.Drawing.Point(116, 220);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(103, 39);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "Verificar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button2.Location = new System.Drawing.Point(250, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl1.Location = new System.Drawing.Point(140, 95);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(53, 17);
+            this.lbl1.TabIndex = 8;
+            this.lbl1.Text = "Lado A";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl2.Location = new System.Drawing.Point(140, 133);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(53, 17);
+            this.lbl2.TabIndex = 9;
+            this.lbl2.Text = "Lado B";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lbl3.Location = new System.Drawing.Point(140, 169);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(53, 17);
+            this.lbl3.TabIndex = 10;
+            this.lbl3.Text = "Lado C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 422);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
+
---
+++ Atividade 4/PTriangulo/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTriangulo
+{
+    public partial class Form1 : Form
+    {
+        double ladoA;
+        double ladoB;
+        double ladoC;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txt1.Text, out ladoA) && double.TryParse(txt2.Text, out ladoB) && double.TryParse(txt3.Text, out ladoC))
+            {
+                if ((ladoA < (ladoB + ladoC)) && (ladoA > Math.Abs(ladoB - ladoC)) && (ladoB < (ladoA + ladoC)) && (ladoB > Math.Abs(ladoA - ladoC)) && (ladoC < (ladoA + ladoB)) && (ladoC > Math.Abs(ladoA - ladoB)))
+                {
+                    if ((ladoA == ladoB) && (ladoB == ladoC) && (ladoC == ladoA))
+                    {
+                        MessageBox.Show("Triângulo Equilátero");
+                    }
+                    else if ((ladoA == ladoB) || (ladoB == ladoC) || (ladoC == ladoA))
+                    {
+                        MessageBox.Show("Triângulo Isóceles");
+                    }
+                    else 
+                    {
+                        MessageBox.Show("Triângulo Escaleno");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
